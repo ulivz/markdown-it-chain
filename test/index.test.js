e9217b907d5ed0daf38cf5f7b724f78002891920
ULIVZ <472590061@qq.com>
2018-08-23 01:53:26 +0800

    test: real world usage testing

--- test/index.test.js
+++ test/index.test.js
@@ -129,5 +129,45 @@ test('plugin - modify options', () => {
   expect(config.plugins[0].args[0]).toEqual({ a: 1, b: 2 })
 })
 
+test('realword example', () => {
+  // Require the markdown-it-chain module. This module exports a single
+  // constructor function for creating a configuration API.
+  const Config = require('markdown-it-chain')
 
+  // Instantiate the configuration with a new API
+  const config = new Config()
+
+  // Make configuration changes using the chain API.
+  // Every API call tracks a change to the stored configuration.
+  config
+    // Interact with 'options' in new MarkdownIt
+    // Ref: https://markdown-it.github.io/markdown-it/#MarkdownIt.new
+    .options
+      .html(true) // equal to .set('html', true)
+      .linkify(true)
+      .end()
+
+    // Interact with 'plugins'
+    .plugin('toc')
+      // The first parameter is the plugin module, which may be a function
+      // while the second parameter is an array of parameters accepted by the plugin.
+      .use(require('markdown-it-table-of-contents'), [{
+        includeLevel: [2, 3]
+      }])
+      // Move up one level, like .end() in jQuery.
+      .end()
+
+    .plugin('anchor')
+      .use(require('markdown-it-anchor'), [{
+        permalink: true,
+        permalinkBefore: true,
+        permalinkSymbol: '$'
+      }])
+      // Apply this plugin before toc.
+      .before('toc')
+
+  // Create a markdown-it instance using the above configuration
+  const md = config.toMd()
+  expect(md.render('[[TOC]] \n # h1 \n ## h2 \n ## h3 ')).toMatchSnapshot()
+})
 
