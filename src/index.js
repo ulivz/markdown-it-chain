e5a60ee2d5b5b3adb06eec138bd2ec33c54f1d27
子肃 <zisu.cx@alibaba-inc.com>
2018-12-26 23:42:51 +0800

    fix: plugins empty error

--- src/index.js
+++ src/index.js
@@ -35,6 +35,9 @@ module.exports = class MarkdownItChain extends ChainedMap {
       markdownIt = require(markdownIt)
     }
     const md = markdownIt(Object.assign(instantiationOptions, options))
+    if (!plugins || plugins.length === 0) {
+      return md;
+    }
     return plugins.reduce((md, { plugin, args }) => md.use(plugin, ...args), md)
   }
 }
